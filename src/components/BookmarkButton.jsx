2d750a03c84bb1ab986e18722195041fdb947976
ritinkar pramanik <ritinkar@outlook.com>
2018-01-24 04:02:48 +0530

    Updated the bookmarkbutton, removed fontawesome dependency

--- src/components/BookmarkButton.jsx
+++ src/components/BookmarkButton.jsx
@@ -1,18 +1,13 @@
 import React from 'react';
-import FontAwesome from 'react-fontawesome';
+import { Icon } from 'semantic-ui-react';
 import { bookmark } from '../actions/bookmark';
 import { connect } from 'react-redux';
 
 class BookmarkButton extends React.Component {
 
     render() {
-        let icon = null;
-        if (this.props.isBookmarked === true) {
-            icon = <FontAwesome name='bookmark' style={styles.active} />
-        }
-        else {
-            icon = <FontAwesome name='bookmark' />
-        }
+        const icon = this.props.isBookmarked === true ? <Icon name='bookmark' /> : <Icon name='remove bookmark' />;
+
         return (
             <div style={{ flex: 1, textAlign: 'center' }} onClick={() => this.props.onClick(this.props.id)}>
                 {icon}
@@ -21,11 +16,6 @@ class BookmarkButton extends React.Component {
     }
 }
 
-const styles = {
-    active: {
-        color: 'red'
-    }
-}
 
 const mapDispatchToProps = (dispatch) => {
     return {
